12ef015d411a05f4bf21f0e878b4dde498811fb8
shubham9404 <shubham279710@gmail.com>
2021-09-08 10:59:39 +0530

    Success

---
+++ GitTut/TableViewCell.swift
@@ -0,0 +1,23 @@
+//
+//  TableViewCell.swift
+//  GitTut
+//
+//  Created by Shubham Shinde on 08/09/21.
+//
+
+import UIKit
+
+class TableViewCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
